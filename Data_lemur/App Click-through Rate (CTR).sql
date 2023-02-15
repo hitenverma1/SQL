07b2527c17c6d4594770f176e9a4deb391d956d6
Hiten Verma <hitenverma1996@gmail.com>
2023-02-15 17:31:07 +0530

    update

--- Data_lemur/App Click-through Rate (CTR).sql
+++ Data_lemur/App Click-through Rate (CTR).sql
@@ -1,4 +1,4 @@
-SELECT   app_id,
+SELECT app_id,
   ROUND(100.0 *
     SUM(CASE WHEN event_type = 'click' THEN 1 ELSE 0 END) /
     SUM(CASE WHEN event_type = 'impression' THEN 1 ELSE 0 END), 2)  AS ctr
