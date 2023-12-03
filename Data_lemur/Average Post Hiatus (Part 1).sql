b66190cbdbb5c5a571385e8197a8361c626b021f
“[Hiten <[hitenverma1996@gmail.com]>
2023-12-03 21:52:24 +0530

    add sql

--- Data_lemur/Average Post Hiatus (Part 1).sql
+++ Data_lemur/Average Post Hiatus (Part 1).sql
@@ -2,3 +2,11 @@ SELECT user_id, (MAX(post_date::date) - MIN(post_date::date)) as days_between FR
 where EXTRACT(year from post_date) = '2021'
 GROUP BY user_id
 HAVING (MAX(post_date::date) - MIN(post_date::date)) <> 0
+
+
+SELECT user_id, 
+EXTRACT(day from (MAX(post_date)- MIN(post_date))) as days_between
+FROM posts
+where EXTRACT(year from post_date) = 2021
+GROUP BY user_id
+HAVING COUNT(post_id)>1;
--- Data_lemur/Teams Power Users.sql
+++ Data_lemur/Teams Power Users.sql
@@ -5,3 +5,10 @@ WHERE EXTRACT(MONTH FROM sent_date) = '8'
 GROUP BY sender_id
 ORDER BY num DESC
 limit 2;
+
+
+SELECT sender_id, COUNT(message_id) as message_count FROM messages
+WHERE EXTRACT(Year FROM sent_date) = 2022 and EXTRACT(month FROM sent_date) = 8
+GROUP BY sender_id
+ORDER BY message_count desc
+limit 2;
